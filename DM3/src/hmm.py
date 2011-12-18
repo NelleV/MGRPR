d902c6a3aaef7d9ed36cd6ae2777ecd59720e005
Nelle Varoquaux <nelle.varoquaux@gmail.com>
2011-12-18 21:41:09 +0100

    separate different computations

--- DM3/src/hmm.py
+++ DM3/src/hmm.py
@@ -14,7 +14,7 @@ a[0] = 0.21
 a[2] = 0.29
 
 pi = 0.5  # Valeur bidon
-mu = gmm.means.copy() 
+mu = gmm.means.copy()
 sigma = gmm.covars
 
 
@@ -22,23 +22,37 @@ def _calculate_normal(X, mu, sigma):
     """
     Calculate the probability p of X following normal law
     """
-    a = 1. / (np.sqrt(2 * np.pi) * np.linalg.det(sigma))
+    a = 1. / (np.sqrt(2 * np.pi) ** len(sigma) * np.sqrt(np.linalg.det(sigma)))
     S_inv = np.linalg.inv(sigma)
-    b = np.dot(np.dot((X - mu), S_inv), (X - mu).T)
+    b = np.dot(np.dot((X - mu).T, S_inv), (X - mu))
     p = a * np.exp(-1. / 2 * b)
     return p
 
 
-def e_step(data, a, pi, mu, sigma):
+def compute_gamma(a, alpha):
     """
     """
-    q = np.zeros((len(data), 1))
-    alpha = np.zeros((len(q), K))
-    alpha_norm = np.zeros((len(q), 1))
-    beta = np.zeros((len(q), K))
-    gamma = np.zeros((len(q), K))
+    K = len(a)
+    gamma = np.zeros((len(alpha), K))
+    for i, element in enumerate(alpha):
+        if i == 0:
+            gamma[len(alpha) - 1 - i] = alpha[len(alpha) - 1 - i]
+        else:
+            for k in range(K):
+                b = alpha[len(alpha) - i, k] * a[k, :]
+                c = (alpha[len(alpha) - i] * a).sum(axis=1)
+                gamma[len(alpha) - 1 - i, k] = np.dot(b / c,
+                                                  gamma[len(alpha) - i])
+    return gamma
 
-    for i, element in enumerate(q):
+
+def compute_alpha(data, pi, mu, sigma):
+    """
+    """
+    alpha = np.zeros((len(data), K))
+    alpha_norm = np.zeros((len(data), 1))
+
+    for i, element in enumerate(data):
         p = np.array([_calculate_normal(data[i, :], mu[j], sigma[j])
                         for j in range(K)])
 
@@ -51,35 +65,18 @@ def e_step(data, a, pi, mu, sigma):
             for k in range(K):
                 alpha[i, k] = np.dot(alpha[i - 1, :] * a[:, k], p)
             alpha_norm[i] = alpha[i, :].sum()
-            alpha[i, k] /= alpha_norm[i]
+            alpha[i, :] /= alpha_norm[i]
+    return alpha
 
-    for i, element in enumerate(q):
-        p = np.array([_calculate_normal(data[len(q) - 1 - i, :],
-                                        mu[j],
-                                        sigma[j])
-                        for j in range(K)])
 
-        if i == 0:
-            beta[len(q) - 1 - i, :] = 0.25, 0.25, 0.25, 0.25
-            beta[len(q) - 1 - i, :] /= alpha_norm[len(q) - 1 - i]
-        else:
-            for k in range(K):
-                beta[len(q) - 1 - i, k] = np.dot(beta[len(q) - i] * a[:, k], p)
-            beta[len(q) - 1 - i, :] /= alpha_norm[len(q) - 1 - i]
+def e_step(data, a, pi, mu, sigma):
+    """
+    """
+    q = np.zeros((len(data), 1))
+    beta = np.zeros((len(q), K))
 
-    for i, element in enumerate(q):
-        p = np.array([_calculate_normal(data[len(q) - 1 - i, :],
-                                        mu[j],
-                                        sigma[j])
-                        for j in range(K)])
-        if i == 0:
-            gamma[len(q) - 1 - i] = alpha[len(q) - 1 - i]
-        else:
-            for k in range(K):
-                b = alpha[len(q) - i] * a[:, k]
-                gamma[len(q) - 1 - i, k] = np.dot(b / b.sum(),
-                                                  gamma[len(q) - i])
-
+    alpha = compute_alpha(data, pi, mu, sigma)
+    gamma = compute_gamma(a, alpha)
     xi = np.zeros((len(q) - 1, K, K))
     for i, element in enumerate(q[:-1]):
         p = np.array([_calculate_normal(data[i + 1, :], mu[j], sigma[j])
@@ -109,7 +106,8 @@ def m_step(gamma, xi, data, sigma, mu, a, pi):
     return pi, a, gamma, mu, sigma
 
 
-max_iter = 10
+max_iter = 1
 for i in range(max_iter):
     alpha, beta, gamma, xi = e_step(data, a, pi, mu, sigma)
     pi, a, gamma, mu, sigma = m_step(gamma, xi, data, sigma, mu, a, pi)
+q = gamma.argmax(axis=1)
